fd354af6e8821ad8103a77c4acee32ab9c78a1f2
翻墙教练 <34854165+gfw-breaker@users.noreply.github.com>
2019-10-24 03:05:17 -0400

    Update convert_cc.sh

--- convert_cc.sh
+++ convert_cc.sh
@@ -11,6 +11,11 @@ fname=$2
 
 youtube-dl -f 18 --restrict-filenames $vid
 
+if [ $? -ne 0 ]; then
+	echo "Failed to download video from YouTube, please check whether the video ID is correct..."
+	exit 2
+fi
+
 mv *-$vid.mp4 $fname.mp4
 
 autosub -F srt -S zh-CN -D zh-CN $fname.mp4
